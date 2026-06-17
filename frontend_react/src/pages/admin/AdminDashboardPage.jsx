adc32348e8610d90a5c82f08ebf8e32c663bb83f
phamhoaithu2006 <phamhoaithu02112006@gmail.com>
2026-06-17 23:57:53 +0700

    fix FE

--- frontend_react/src/pages/admin/AdminDashboardPage.jsx
+++ frontend_react/src/pages/admin/AdminDashboardPage.jsx
@@ -1,19 +1,3 @@
-import { Route, Routes } from 'react-router-dom'
-
-import AdminLayout from '../../layouts/AdminLayout'
-import TourTypeCreatePage from './categories/TourTypeCreatePage'
-import TourTypeEditPage from './categories/TourTypeEditPage'
-import TourTypeListPage from './categories/TourTypeListPage'
-import TourTypeTrashPage from './categories/TourTypeTrashPage'
-
-function DashboardHome() {
-  return (
-    <section className="admin-page-header">
-      <p>Tổng quan vận hành</p>
-      <h1>Admin Dashboard</h1>
-    </section>
-  )
-}
 import { Navigate, Route, Routes } from 'react-router-dom'
 import AdminLayout from '../../layouts/AdminLayout'
 import GuideManagementPage from './GuideManagementPage'
@@ -22,11 +6,6 @@ function AdminDashboardPage() {
   return (
     <AdminLayout>
       <Routes>
-        <Route index element={<DashboardHome />} />
-        <Route path="tours" element={<TourTypeListPage />} />
-        <Route path="tours/create" element={<TourTypeCreatePage />} />
-        <Route path="tours/:id/edit" element={<TourTypeEditPage />} />
-        <Route path="tours/trash" element={<TourTypeTrashPage />} />
         <Route
           index
           element={
