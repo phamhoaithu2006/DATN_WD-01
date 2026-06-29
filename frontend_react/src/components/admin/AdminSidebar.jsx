1ec63bc705b9dc16c851faa940a0c93022ad6e2e
HoangVu08122006 <tama200006@gmail.com>
2026-06-30 00:16:51 +0700

    Gui thong bao admin

--- frontend_react/src/components/admin/AdminSidebar.jsx
+++ frontend_react/src/components/admin/AdminSidebar.jsx
@@ -115,6 +115,16 @@ const menuItems = [
     ),
   },
   {
+    label: 'Thông Báo',
+    path: '/admin/notifications',
+    icon: (
+      <>
+        <path d="M18 8a6 6 0 0 0-12 0c0 7-3 7-3 9h18c0-2-3-2-3-9" />
+        <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+      </>
+    ),
+  },
+  {
     label: 'Cài Đặt Hệ Thống',
     path: '/admin/settings',
     icon: (
