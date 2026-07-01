ec6c4456f8ae00f222cc7797dc039ca3928bd41a
nvthang2006 <nvtt135@gmail.com>
2026-07-01 10:02:21 +0700

    fix conflict

--- frontend_react/src/main.jsx
+++ frontend_react/src/main.jsx
@@ -9,11 +9,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
       <Toaster richColors position="top-right" />
-=======
-    <Toaster richColors position="top-right" />
->>>>>>> origin/main
       <App />
     </BrowserRouter>
   </StrictMode>,
